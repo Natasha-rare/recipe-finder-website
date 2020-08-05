1eefa0ccc9a8eace34f8960d88a7e61f64bf9dda
Natasha-rare <Natasha_ea5@mail.ru>
2020-08-05 21:38:46 +0300

    mac update

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/')  # main page
 @app.route('/index')
 def index():
     return render_template("main.html")
 
-@app.route("/contacts")
+
+@app.route("/contacts")  # page contacts
 def contacts():
     return render_template("contacts.html")
+
+
+@app.route("/policy")  # page Privacy Policy
+def policy():
+    return render_template("privacyPolicy.html")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
